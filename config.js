3545a16b77d86d426a45cebb833a6a90658ccf75
Frank Cheng <ufo22940268@gmail.com>
2019-06-18 09:19:25 +0800

    v

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
  */
 "use strict";
 
-const host = String.raw`http://192.168.31.77:3000`
+const host = process.env.NODE_ENV === 'production' ? "http://106.12.82.179:3000" : String.raw`http://192.168.31.77:3000`;
 
 function composeImageUrl(fileName) {
   return host +'/image/' + fileName
